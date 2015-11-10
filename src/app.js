260f4cdb6095e49ad6c48d9e7ee714517c93bda7
Lucas <lucas@agtlucas.com>
2015-11-10 10:48:00 -0200

    Adding src/app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hey, {this.props.name}!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App name="Lucas" />, document.getElementById('main__root'));
