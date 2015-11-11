6d99be51b401022c681ad07326ea9e73d5493d3c
Lucas <lucas@agtlucas.com>
2015-11-11 15:07:49 -0200

    Update webpack and cia

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return(
       <div><h1>Hey, {this.props.name}!</h1></div>
     );
   }
 }
-
-ReactDOM.render(<App name="Lucas" />, document.getElementById('main__root'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/app'
+    './src/index'
   ],
 
   output: {
