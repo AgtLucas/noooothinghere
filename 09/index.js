88d6a8fb498f8d7e37c497e3a8e158428069dc46
Lucas <lucas@agtlucas.com>
2016-06-06 09:41:08 +0200

    Update 09

--- 09/index.js
+++ 09/index.js
@@ -3,6 +3,11 @@ const addCounter = (list) => {
   return [...list, 0]
 }
 
+const removeCounter = (list, index) => {
+  list.splice(index, 1)
+  return list
+}
+
 const testCounter = () => {
   const listBefore = []
   const listAfter = [0]
@@ -11,6 +16,15 @@ const testCounter = () => {
 
   expect(
     addCounter(listBefore)
+  ).toEqual(listAfter)
+}
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 20]
+
+  expect(
+    removeCounter(listBefore, 1)
   ).toEqual(listAfter)
 }
 
