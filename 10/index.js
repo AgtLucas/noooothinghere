38420b1758cd9cc413c95c05c42234c05ef95366
Lucas <lucas@agtlucas.com>
2016-06-08 07:19:32 -0300

    Update 10

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,23 @@
+const toggleTodo = (todo) => {
 
+}
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  }
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter)
+}
+
+testToggleTodo()
+console.log('All tests passed.')
