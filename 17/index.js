e94e774bb0861226c1d0a355dc38d380019f3862
Lucas <lucas@agtlucas.com>
2016-06-12 09:53:15 -0300

    Update 17

--- 17/index.js
+++ 17/index.js
@@ -54,3 +54,37 @@ const todoApp = combineReducers({
 
 const { createStore } = Redux
 const store = createStore(todoApp)
+
+const { Component } = React
+
+let nextTodoId = 0
+class TodoApp extends Component {
+  render () {
+    return (
+      <div>
+        <button onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: 'Test',
+              id: nextTodoId++
+            })
+          }}>Add Todo</button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}>{todo.text}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root')
+  )
+}
+
+store.subscribe(render)
+render()
